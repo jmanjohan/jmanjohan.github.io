9677403d21285057c2c7c253234f741ff3a4025c
jmanjohan <jmanjohan@yahoo.com>
2018-03-09 14:52:29 -0600

    jquery trial question

---
+++ script/main.js
@@ -0,0 +1,8 @@
+$('.homeProfile').on('mouseleave mouseup', function() {
+    $(this).addClass('withBoxShadow');
+    $('.click').remove();
+});
+$('.homeProfile').mousedown(function() {
+    $(this).removeClass('withBoxShadow');
+    $(this).prepend('<div class="click"></div>');
+});
