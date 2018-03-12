371b608c510036d7aa61f129431e384146c93864
jmanjohan <jmanjohan@yahoo.com>
2018-03-12 18:59:14 -0500

    add links

--- script/main.js
+++ script/main.js
@@ -1,8 +1,25 @@
+//inset shadow when clicking profiles/buttons on the home page
 $('.homeProfile').on('mouseleave mouseup', function() {
     $(this).addClass('withBoxShadow');
-    $('.click').remove();
+    $('.clickShadow').remove();
 });
-$('.homeProfile').mousedown(function() {
+$('.siteRunnerButton').on('mouseleave mouseup', function() {
+    $('.clickShadow').remove();
+});
+$('.canClick').mousedown(function() {
     $(this).removeClass('withBoxShadow');
-    $(this).prepend('<div class="click withBorderRoundThinChild"></div>');
+    $(this).prepend('<div class="clickShadow withBorderRoundThinChild"></div>');
+});
+//navigate site by clicking profiles
+$('#justinProfile').mouseup(function() {
+    $('#justinProfileLink')[0].click();
+});
+$('#joshuaProfile').mouseup(function() {
+    $('#joshuaProfileLink')[0].click();
+});
+$('#jessicaProfile').mouseup(function() {
+    $('#jessicaProfileLink')[0].click();
+});
+$('#jacobProfile').mouseup(function() {
+    $('#jacobProfileLink')[0].click();
 });
